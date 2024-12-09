42d93bc6c929e1b2b07e929ec7ba672eda91520a
syedabanog-1 <gulzar_bano@yahoo.com>
2024-12-10 01:11:07 +0500

    commit done

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
+import './globals.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <head></head>
+        <body>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
